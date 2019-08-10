e01943c342b7254437da21e4059dc56a5c3de3d5
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-08-10 11:00:20 -0400

    stack push and pop

--- rl/datastructs/stack/ArrayStack.py
+++ rl/datastructs/stack/ArrayStack.py
@@ -5,3 +5,10 @@ class Stack(object):
 
     def get_length(self):
         return self.__stack.__len__()
+
+    def push(self, data):
+        self.__stack.append(data)
+
+    def pop(self):
+        if self.get_length() > 0 :
+            return self.__stack.pop(0)
--- rl/tests/stack/ArrayStackTests.py
+++ rl/tests/stack/ArrayStackTests.py
@@ -5,6 +5,25 @@ from rl.datastructs.stack.ArrayStack import Stack
 
 class StackTests(unittest.TestCase):
 
+    def setUp(self):
+        self.stack = Stack()
+
     def test_create_empty(self):
-        stack = Stack()
-        self.assertEqual(0, stack.get_length())
+
+        self.assertEqual(0, self.stack.get_length())
+
+    def test_stack_push(self):
+        self.stack.push(1)
+
+        self.assertEqual(1, self.stack.get_length())
+
+    def test_pop_stacl(self):
+        self.stack.push(1)
+        data = self.stack.pop()
+
+        self.assertEqual(1, data)
+
+    def test_pop_stack_empty(self):
+        data = self.stack.pop()
+
+        self.assertIsNone(data)
