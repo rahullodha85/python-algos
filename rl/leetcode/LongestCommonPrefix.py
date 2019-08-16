411d2344f570180243221486fc870a7f5b993f24
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-08-16 15:41:33 -0400

    more test cases

--- rl/leetcode/LongestCommonPrefix.py
+++ rl/leetcode/LongestCommonPrefix.py
@@ -5,13 +5,20 @@ class LongestCommonPrefix(object):
             return strs[0]
         if strs.__len__() == 0:
             return ''
+        min_length = -1
+        for index in range(strs.__len__()):
+            if min_length == -1:
+                min_length = strs[index].__len__()
+            else:
+                min_length = min(strs[index].__len__(), min_length)
         for index1 in range(1, strs.__len__(), 1):
             str1 = strs[index1 - 1]
             str2 = strs[index1]
             if str1 == '' or str2 == '':
                 return ''
-            short = min(strs[index1].__len__(), strs[index1 - 1].__len__())
-            for index2 in range(short):
+            # short = min(strs[index1].__len__(), strs[index1 - 1].__len__())
+
+            for index2 in range(min_length):
                 if index2 < common_prefix.__len__():
                     if str2[index2] != common_prefix[index2]:
                         common_prefix = common_prefix[0:index2]
@@ -21,5 +28,7 @@ class LongestCommonPrefix(object):
                         common_prefix = common_prefix + str1[index2]
                     else:
                         break
+            if common_prefix == '':
+                return ''
 
         return common_prefix
