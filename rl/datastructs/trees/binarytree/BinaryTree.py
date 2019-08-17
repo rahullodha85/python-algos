44acf1538cfa1a6432d60fe1983aea1d7f629a33
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-08-17 13:40:29 -0400

    is empty function and other refactoring

--- rl/datastructs/trees/binarytree/BinaryTree.py
+++ rl/datastructs/trees/binarytree/BinaryTree.py
@@ -1,5 +1,19 @@
 class TreeNode:
-    def __init__(self, data=None):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
+
+
+class BinaryTree:
+
+    def __init__(self, data=None):
+        if data:
+            self.root = TreeNode(data)
+        else:
+            self.root = None
+
+    def is_empty(self) -> bool:
+        return self.root is None
+
+
--- rl/tests/trees/binarytree/BinaryTreeTests.py
+++ rl/tests/trees/binarytree/BinaryTreeTests.py
@@ -1,18 +1,22 @@
 import unittest
 
-from rl.datastructs.trees.binarytree.BinaryTree import TreeNode
+from rl.datastructs.trees.binarytree.BinaryTree import TreeNode, BinaryTree
 
 
 class BinaryTreeTests(unittest.TestCase):
 
     def test_init_empty(self):
-        tree_node = TreeNode()
-        self.assertIsNone(tree_node.data)
-        self.assertIsNone(tree_node.left)
-        self.assertIsNone(tree_node.right)
+        tree = BinaryTree()
+        self.assertIsNone(tree.root)
 
     def test_init(self):
-        tree_node = TreeNode(2)
-        self.assertEqual(2, tree_node.data)
-        self.assertIsNone(tree_node.left)
-        self.assertIsNone(tree_node.right)
+        tree = BinaryTree(2)
+        self.assertEqual(2, tree.root.data)
+        self.assertIsNone(tree.root.left)
+        self.assertIsNone(tree.root.right)
+
+    def test_is_empty_empty_tree(self):
+        self.assertTrue(BinaryTree().is_empty())
+
+    def test_is_empty_not_empty_tree(self):
+        self.assertFalse(BinaryTree(2).is_empty())
